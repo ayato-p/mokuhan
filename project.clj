a008db05470de57eadb604f4543ad775eb44e65e
ayato_p <ayato.skycrawlers@gmail.com>
2018-03-26 07:28:37 +0900

    Setup CI

--- project.clj
+++ project.clj
@@ -18,7 +18,8 @@
    :dev
    {:dependencies [[cheshire "5.8.0"]
                    [clj-http "3.8.0"]
-                   [com.cemerick/piggieback "0.2.2"]]}
+                   [com.cemerick/piggieback "0.2.2"]
+                   [doo "0.1.10"]]}
 
    :cljstest
    [:plugin/cljsbuild :plugin/doo]
@@ -28,14 +29,23 @@
     :cljsbuild
     {:builds
      {:node-test {:source-paths ["src" "test"]
-                  :compiler {:output-to "target/test/org/panchromatic/mokuhan-test.js"
-                             :output-dir "target/test/org/panchromatic"
+                  :compiler {:output-to "target/node-test/org/panchromatic/mokuhan-test.js"
+                             :output-dir "target/node-test/org/panchromatic"
                              :main org.panchromatic.mokuhan.test-runner
                              :optimizations :advanced
-                             :target :nodejs}}}}}
+                             :target :nodejs}}
+
+      :phantom-test {:source-paths ["src" "test"]
+                     :compiler {:output-to "target/phantom-test/org/panchromatic/mokuhan-test.js"
+                                :output-dir "target/phantom-test/org/panchromatic"
+                                :main org.panchromatic.mokuhan.test-runner
+                                :optimizations :advanced}}}}}
 
    :plugin/doo
    {:plugins [[lein-doo "0.1.10"]]}}
 
   :aliases
-  {"cljstest" ["with-profile" "+cljstest" "doo" "node" "node-test" "once"]})
+  {"cljstest-node" ["doo" "node" "node-test" "once"]
+   "cljstest-phantom" ["doo" "phantom" "phantom-test" "once"]
+   "cljstest" ["with-profile" "+cljstest" "do" ["cljstest-node"] ["cljstest-phantom"]]
+   "test-all" ["do" ["cljstest"] ["test"]]})
--- test/org/panchromatic/mokuhan/test_runner.cljs
+++ test/org/panchromatic/mokuhan/test_runner.cljs
@@ -1,11 +1,11 @@
 (ns org.panchromatic.mokuhan.test-runner
-  (:require [cljs.test :as t :include-macros true]
+  (:require [doo.runner :refer-macros [doo-tests]]
             org.panchromatic.mokuhan-test
-            org.panchromatic.mokuhan.walker-test
-            org.panchromatic.mokuhan.renderer-test))
+            org.panchromatic.mokuhan.renderer-test
+            org.panchromatic.mokuhan.walker-test))
 
 (enable-console-print!)
 
-(t/run-tests 'org.panchromatic.mokuhan.walker-test
-             'org.panchromatic.mokuhan.renderer-test
-             'org.panchromatic.mokuhan-test)
+(doo-tests 'org.panchromatic.mokuhan.walker-test
+           'org.panchromatic.mokuhan.renderer-test
+           'org.panchromatic.mokuhan-test)
