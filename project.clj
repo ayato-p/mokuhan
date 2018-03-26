8f6de17b5c4a3712aa83ba4f37234de86f3c630b
ayato_p <ayato.skycrawlers@gmail.com>
2018-03-26 18:05:56 +0900

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.panchromatic/mokuhan "0.1.1"
+(defproject org.panchromatic/mokuhan "0.1.2-SNAPSHOT"
   :description "Yet another implementation of Mustache in Clojure."
   :url "https://github.com/ayato-p/mokuhan"
   :license {:name "MIT License"
